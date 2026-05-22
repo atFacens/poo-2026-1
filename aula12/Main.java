c2455f48944414331653fbf20072760007b274cd
Emerson Paduan <emerson@paduan.dev.br>
2026-05-21 23:15:49 -0300

    aula 12

---
+++ aula12/Main.java
@@ -0,0 +1,20 @@
+
+import br.com.empresa.MinhaClasse;
+
+public class Main {
+    
+
+    public static void main(String[] args) {
+        // MinhaClasse m1 = new MinhaClasse();
+        // MinhaClasse m2 = new MinhaClasse();
+
+        MinhaClasse.calculaIdade();
+
+        MinhaClasse.numero = 10;
+        MinhaClasse.numero = 20;
+
+        System.out.println(MinhaClasse.numero);
+        System.out.println(MinhaClasse.numero);
+
+    }
+}
---
+++ aula12/br/com/empresa/MinhaClasse.java
@@ -0,0 +1,9 @@
+package br.com.empresa;
+
+public class MinhaClasse {
+    public static int numero;
+    
+    public static int calculaIdade() {
+        return 12;
+    }
+}
